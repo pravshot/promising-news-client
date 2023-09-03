d294e15135e5b7fd11a0f6e1c4ff76b556b8f604
pravshot <praveenkalva@icloud.com>
2023-09-03 19:39:31 -0400

    updated url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:4000/news";
+const url = "https://promising-news.uc.r.appspot.com/news";
 
 export const getAllNews = async () => await axios.get(url);
 export const getNewsWithParams = async (params) =>
